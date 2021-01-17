fec0434c4edb51577f5609b9f58785cbf7aaf80f
ALİ AÇIKYOL <aliacikyol6301@hotmail.com>
2021-01-17 23:27:42 +0300

    Add project files.

---
+++ SoftwareCamp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SoftwareCamp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety = tip güvenliği 
+            //Do not repeat yourself - Kendini tekrarlama
+
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine();
+            }
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+        }
+    }
+}
