b1c920b678b77b08c0e0567d1b1a884efcbf39e4
ALİ AÇIKYOL <aliacikyol6301@hotmail.com>
2021-01-22 23:22:44 +0300

    Operators 4.gün(1)

---
+++ OOP1/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class Product
+    {
+        
+        public int Id { get; set; } //İd'si
+        public int CategoryId { get; set; } //Ürün kategorisi
+        public string ProductName { get; set; } //Ürün İsmi
+        public double UnitPrice { get; set; } //Fiyatı
+        public int UnitsInStock { get; set; } //Stok adedi
+
+
+
+    }
+}
